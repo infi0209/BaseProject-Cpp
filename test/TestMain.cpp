48ea74625ec3b0d7c85010067dbdb60cececc747
infi0209 <infi0209@gmail.com>
2017-12-22 18:54:00 +0800

    呷土豆啦!!

--- test/TestMain.cpp
+++ test/TestMain.cpp
@@ -3,6 +3,10 @@
 
 void testGetTriangleType() {
     assert(getTriangleType(3, 4, 5) == 0);
+    assert(getTriangleType(5, 4, 3) == 0);
+    assert(getTriangleType(3, 5, 5) == 1);
+    assert(getTriangleType(3, 5, 6) == -1);
+    assert(getTriangleType(3, 4, 8) == -2);
 }
 
 int main() {
